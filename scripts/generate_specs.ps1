60983183d5b6dfbb305e62d1d1182e2f281e22c5
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-11-26 08:28:42 -0800

    stash specgen

--- scripts/generate_specs.ps1
+++ scripts/generate_specs.ps1
@@ -7,11 +7,30 @@ $RepoRoot = Convert-Path "$PSScriptRoot/.."
 $SpecTestRoot = [IO.Path]::Combine($RepoRoot, "vendor", "webassembly", "test", "core")
 $DestRoot = [IO.Path]::Combine($RepoRoot, "tests", "spec")
 
+# Right now, manually specify the tests to generate Rust tests for
+# This is because we don't implement everything yet.
+$TestsToGenerate = @(
+    "i32"
+)
+
 Get-ChildItem $SpecTestRoot -Filter *.wast | ForEach-Object {
-    Write-Host "Processing $($_.Name)"
     $name = [IO.Path]::GetFileNameWithoutExtension($_.FullName)
-    $DestDir = Join-Path $DestRoot $name
-    mkdir $Dest | Out-Null
-    $Dest = Join-Path $DestDir "$name.json"
-    wast2json $_.FullName -o $Dest
+    if($TestsToGenerate -icontains $name) {
+        Write-Host "Processing $($_.Name)"
+        $DestDir = Join-Path $DestRoot $name
+        if(Test-Path $DestDir) {
+            Remove-Item -Recurse -Force $DestDir
+        }
+        mkdir $DestDir | Out-Null
+        $Dest = Join-Path $DestDir "$name.json"
+        wast2json $_.FullName -o $Dest
+
+        # Now process the spec into a Rust test using the specgen command
+        pushd $RepoRoot
+        try {
+            cargo run --bin specgen -- $Dest
+        } finally {
+            popd
+        }
+    }
 }
