5c76a4184fb4f10cf6e41f88852996141b136737
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-11-26 09:43:37 -0800

    got some of it working

--- scripts/generate_specs.ps1
+++ scripts/generate_specs.ps1
@@ -6,6 +6,7 @@ if (!(Get-Command wast2json -ErrorAction SilentlyContinue)) {
 $RepoRoot = Convert-Path "$PSScriptRoot/.."
 $SpecTestRoot = [IO.Path]::Combine($RepoRoot, "vendor", "webassembly", "test", "core")
 $DestRoot = [IO.Path]::Combine($RepoRoot, "tests", "spec")
+$SpecGenRoot = [IO.Path]::Combine($RepoRoot, "specgen")
 
 # Right now, manually specify the tests to generate Rust tests for
 # This is because we don't implement everything yet.
@@ -13,24 +14,29 @@ $TestsToGenerate = @(
     "i32"
 )
 
-Get-ChildItem $SpecTestRoot -Filter *.wast | ForEach-Object {
-    $name = [IO.Path]::GetFileNameWithoutExtension($_.FullName)
-    if($TestsToGenerate -icontains $name) {
-        Write-Host "Processing $($_.Name)"
-        $DestDir = Join-Path $DestRoot $name
-        if(Test-Path $DestDir) {
-            Remove-Item -Recurse -Force $DestDir
-        }
-        mkdir $DestDir | Out-Null
-        $Dest = Join-Path $DestDir "$name.json"
-        wast2json $_.FullName -o $Dest
+Push-Location $SpecGenRoot
+try {
+    # Build specgen
+    Write-Host -ForegroundColor Green "Building specgen..."
+    cargo build
 
-        # Now process the spec into a Rust test using the specgen command
-        pushd $RepoRoot
-        try {
-            cargo run --bin specgen -- $Dest
-        } finally {
-            popd
+    Get-ChildItem $SpecTestRoot -Filter *.wast | ForEach-Object {
+        $name = [IO.Path]::GetFileNameWithoutExtension($_.FullName)
+        if ($TestsToGenerate -icontains $name) {
+            Write-Host "Processing $($_.Name)"
+            $DestDir = Join-Path $DestRoot $name
+            if (Test-Path $DestDir) {
+                Remove-Item -Recurse -Force $DestDir
+            }
+            mkdir $DestDir | Out-Null
+            $Dest = Join-Path $DestDir "$name.json"
+            wast2json $_.FullName -o $Dest
+
+            # Now process the spec into a Rust test using the specgen command
+            & "$SpecGenRoot\target\debug\specgen.exe" $Dest
         }
     }
+}
+finally {
+    Pop-Location
 }
